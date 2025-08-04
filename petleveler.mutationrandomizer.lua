58ca4058a512b98a176a94035c7acc165edd6c49
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 17:33:04 +0800

    Update petleveler.mutationrandomizer.lua

--- petleveler.mutationrandomizer.lua
+++ petleveler.mutationrandomizer.lua
@@ -1,15 +1,21 @@
-local scripts = {
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/marxk/refs/heads/main/petleveler.mutationrandomizer.lua"))(),
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))(),
-    [[print("Script 3 executed!")]],
-    [[print("You can keep adding more scripts!")]]
+-- Make sure your executor supports loadstring and HttpGet
+local urls = {
+    "ttps://pastefy.app/SamFj5lo/raw", -- replace with your actual link
+    "https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua", -- another script link
 }
 
-for _, code in ipairs(scripts) do
+for _, url in ipairs(urls) do
     local success, err = pcall(function()
-        loadstring(code)()
+        local code = game:HttpGet(url)
+        local func = loadstring(code)
+        if func then
+            func()
+        else
+            warn("Failed to load code from:", url)
+        end
     end)
+
     if not success then
-        warn("Error in script:", err)
+        warn("Error while running code from:", url, "\n", err)
     end
 end
